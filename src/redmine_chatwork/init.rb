35c883d9dff1ab8b0b8581266cdcf32aaa44eef8
堀祐磨 <hori_yuma@liginc.co.jp>
2016-10-27 21:32:47 +0900

    Fix initial value data type

--- src/redmine_chatwork/init.rb
+++ src/redmine_chatwork/init.rb
@@ -8,17 +8,17 @@ Redmine::Plugin.register :redmine_chatwork do
 	url 'https://github.com/hokkey/redmine_chatwork'
 	author_url 'https://github.com/hokkey/'
 	description 'ChatWork integration'
-	version '0.1'
+	version '0.1.1'
 
-	requires_redmine :version_or_higher => '0.8.0'
+	requires_redmine :version_or_higher => '3.2.0'
 
 	settings \
 		:default => {
 			'room' => nil,
       'token' => nil,
       'chatwork_url' => 'https://api.chatwork.com/v1/rooms/',
-      'post_updates' => 1,
-      'post_wiki_updates' => 1
+      'post_updates' => '1',
+      'post_wiki_updates' => '1'
 		},
 		:partial => 'settings/chatwork_settings'
 end
